0bd6a69429fdfb39e7769f17079fb31cda031a98
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-06 18:26:21 +0900

    백버튼 누를시 앱 꺼짐

--- presentation/src/main/java/com/example/lost_android/ui/base/BaseActivity.kt
+++ presentation/src/main/java/com/example/lost_android/ui/base/BaseActivity.kt
@@ -1,16 +1,21 @@
 package com.example.lost_android.ui.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+
 
 abstract class BaseActivity<B: ViewDataBinding>(
     @LayoutRes private val layoutId: Int
 ): AppCompatActivity() {
 
     protected lateinit var binding: B
+
+    private val finishTime = 1000
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,4 +27,17 @@ abstract class BaseActivity<B: ViewDataBinding>(
     }
 
     abstract fun createView()
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - backPressedTime
+
+        if (intervalTime in 0..finishTime) {
+            finish()
+        } else {
+            backPressedTime = tempTime
+            Toast.makeText(applicationContext, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
