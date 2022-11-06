3bd79176fe72b02d7633cdcf9e867f9cf898d726
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-06 18:17:36 +0900

    조건에 따라 다르게 화면 전환

--- presentation/src/main/java/com/example/lost_android/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/example/lost_android/ui/splash/SplashActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Handler
 import com.example.lost_android.ui.base.BaseActivity
 import com.example.lost_android.ui.main.MainActivity
+import com.example.lost_android.ui.start.StartActivity
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivitySplashBinding
 
 class SplashActivity: BaseActivity<ActivitySplashBinding> (R.layout.activity_splash) {
     override fun createView() {
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            if (true) {
+                startActivity(Intent(this, StartActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            } else {
+                startActivity(Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            }
             finish()
         }, 1000)
     }
