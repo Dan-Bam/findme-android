11440a6bb2121c98be82e6151a8d848e47be8fab
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-06 15:59:54 +0900

    패키징

---
+++ presentation/src/main/java/com/example/lost_android/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lost_android.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lost_android.ui.base.BaseActivity
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding> (R.layout.activity_main) {
+    override fun createView() {
+
+    }
+}
