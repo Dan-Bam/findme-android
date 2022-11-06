d6b5b96125aeeade11815466195e39ae56f67c15
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-06 22:12:02 +0900

    로그인 퍼블리싱

--- presentation/src/main/java/com/example/lost_android/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/example/lost_android/ui/login/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.lost_android.ui.login
 
+import android.content.Intent
+import android.view.View
+import androidx.navigation.findNavController
 import com.example.lost_android.ui.base.BaseActivity
+import com.example.lost_android.ui.register.RegisterActivity
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityLoginBinding
 
 class LoginActivity: BaseActivity<ActivityLoginBinding> (R.layout.activity_login) {
     override fun createView() {
 
+    }
+
+    fun click(view: View) {
+        when (view.id) {
+            R.id.backBtn, R.id.backTxt -> finish()
+            R.id.registerBtn -> {
+                startActivity(Intent(this, RegisterActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+                finish()
+            }
+        }
     }
 }
