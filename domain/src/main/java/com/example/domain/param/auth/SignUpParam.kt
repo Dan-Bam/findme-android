398c299dd777628dcf011b661f368853993c1c8b
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-07 23:46:27 +0900

    SignUpParam으로 변경

---
+++ domain/src/main/java/com/example/domain/param/auth/SignUpParam.kt
@@ -0,0 +1,9 @@
+package com.example.domain.param.auth
+
+data class SignUpParam(
+    var id: String,
+    var password: String,
+    var phone: String,
+    var name: String,
+    var address: String
+)
