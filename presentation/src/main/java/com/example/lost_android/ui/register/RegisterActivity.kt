c4376ad3026b7eade8b5b0025906d9f9c70a6363
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-11 23:23:37 +0900

    키보드 내리기 적용

--- presentation/src/main/java/com/example/lost_android/ui/register/RegisterActivity.kt
+++ presentation/src/main/java/com/example/lost_android/ui/register/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lost_android.ui.register
 
 import com.example.lost_android.ui.base.BaseActivity
+import com.example.lost_android.util.keyboardHide
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,6 +9,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RegisterActivity: BaseActivity<ActivityRegisterBinding> (R.layout.activity_register) {
     override fun createView() {
-
+        binding.registerContainer.setOnTouchListener { _, _ ->
+            keyboardHide(this)
+            false
+        }
     }
 }
