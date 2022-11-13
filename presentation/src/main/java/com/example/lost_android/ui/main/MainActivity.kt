41548b6431acd2323cb19b0bae0ca538aefa77bd
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-13 09:04:58 +0900

    bottomNavigation 연결

--- presentation/src/main/java/com/example/lost_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/example/lost_android/ui/main/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.lost_android.ui.main
 
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lost_android.ui.base.BaseActivity
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding> (R.layout.activity_main) {
     override fun createView() {
         binding.bottomNav.itemIconTintList = null
+        settingBottomNav()
+    }
+
+    private fun settingBottomNav(){
+        val navController = supportFragmentManager.findFragmentById(R.id.mainContainer)?.findNavController()
+        val nav = binding.bottomNav as BottomNavigationView
+        navController?.let {
+            nav.setupWithNavController(navController)
+        }
     }
 }
