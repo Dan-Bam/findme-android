11b00484cf6f172c8bbc7763c627c41a90574470
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-26 21:00:08 +0900

    bitmap을 VectorDrawable로 변경하는 함수

--- presentation/src/main/java/com/example/lost_android/util/Event.kt
+++ presentation/src/main/java/com/example/lost_android/util/Event.kt
@@ -61,6 +61,22 @@ fun keyboardShow(context: Activity) {
     )
 }
 
+fun getBitmapFromVectorDrawable(context: Context, drawableId: Int): Bitmap {
+    var drawable = ContextCompat.getDrawable(context, drawableId)
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        drawable = DrawableCompat.wrap(drawable!!).mutate()
+    }
+    val bitmap = Bitmap.createBitmap(
+        drawable!!.intrinsicWidth,
+        drawable!!.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
+}
+
 fun checkPermission(context: Activity, permissions: List<String>): Boolean {
     permissions.forEach {
         val permission = ContextCompat.checkSelfPermission(context, it)
