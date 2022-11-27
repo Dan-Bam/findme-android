e2cb85f27bd410b0c41a0f75e24ce4921b8a3d28
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-27 10:01:37 +0900

    위치 권한 여부에 따른 지도 로직

--- presentation/src/main/java/com/example/lost_android/ui/component/map/MapFragment.kt
+++ presentation/src/main/java/com/example/lost_android/ui/component/map/MapFragment.kt
@@ -3,7 +3,6 @@ package com.example.lost_android.ui.component.map
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
 import android.location.LocationManager
 import android.view.View
 import android.widget.Toast
@@ -21,6 +20,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 
@@ -28,57 +28,111 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
     private val mapViewModel by activityViewModels<MapViewModel>()
     private val mainViewModel by activityViewModels<MainViewModel>()
     private lateinit var mMap: GoogleMap
-    private lateinit var currentLatLng: LatLng
+    private var currentMarker: Marker? = null
+    private var currentLatLng: LatLng? = null
     private lateinit var clusterManager: ClusterManager<MapData>
 
     override fun createView() {
-        binding.mapFragment = this
-        this.checkPermission()
-        mapViewModel.findAll()
-        mainViewModel.setTitle(getString(R.string.findItem))
         binding.map.apply {
             onCreate(savedInstanceState)
             getMapAsync(this@MapFragment)
         }
+        binding.mapFragment = this
+        mapViewModel.findAll()
+        mainViewModel.setTitle(getString(R.string.findItem))
     }
 
     fun click(view: View) {
-        when(view.id) {
-            R.id.myLocationBtn -> {mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))}
+        when (view.id) {
+            R.id.myLocationBtn -> {
+                if (currentLatLng != null) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng!!))
+                } else {
+                    Toast.makeText(context, "위치권한을 허락해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
     private fun observeMap() = mapViewModel.mapData.observe(this) {
         it.forEach {
-            clusterManager.addItem(MapData(it.latitude.toDouble(), it.longitude.toDouble(), it.title, it.description))
+            clusterManager.addItem(
+                MapData(
+                    it.latitude.toDouble(),
+                    it.longitude.toDouble(),
+                    it.title,
+                    it.description
+                )
+            )
         }
         mMap.setOnCameraIdleListener(clusterManager)
     }
 
     override fun onMapReady(p0: GoogleMap) {
+        this.checkPermission()
         mMap = p0
         mMap.apply {
             clusterManager = ClusterManager<MapData>(requireContext(), mMap)
-            addMarker(MarkerOptions().position(currentLatLng).icon(BitmapDescriptorFactory.fromBitmap(
-                getBitmapFromVectorDrawable(requireContext(), R.drawable.ic_my_location)!!
-            )))
-            moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17f))
             setMinZoomPreference(8f)
             setMaxZoomPreference(17f)
             setOnCameraIdleListener(clusterManager)
         }
+        initMap()
         observeMap()
     }
 
+    private fun initMap() = mMap.apply {
+        if (currentMarker != null) {
+            currentMarker!!.remove()
+        }
+        currentMarker = this.addMarker(MarkerOptions().position(currentLatLng ?: LatLng(37.5662952,126.97794509999994)).icon(BitmapDescriptorFactory.fromBitmap(
+            getBitmapFromVectorDrawable(requireContext(), R.drawable.ic_my_location)!!
+        )))!!
+        moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                currentLatLng ?: LatLng(
+                    37.5662952,
+                    126.97794509999994
+                ), 17f
+            )
+        )
+    }
+
     private fun checkPermission() {
-        if (checkPermission(requireActivity(), listOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))) {
-            val locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-            currentLatLng = if (location == null) {
-                Toast.makeText(context, "위치를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
-                LatLng(37.5662952,126.97794509999994)
-            } else {
-                LatLng(location.latitude, location.longitude)
+        if (checkPermission(
+                this,
+                listOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        ) {
+            getLocation()
+        }
+    }
+
+    private fun getLocation() {
+        val locationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        currentLatLng = if (location == null) {
+            Toast.makeText(context, "위치를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+            LatLng(37.5662952, 126.97794509999994)
+        } else {
+            LatLng(location.latitude, location.longitude)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        grantResults.forEach {
+            if (it == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+                initMap()
             }
         }
     }
