37a6639e2a0aa45590cb3e51bd0800f57d5b4d53
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-28 10:06:08 +0900

    등록 페이지 퍼블리싱

---
+++ presentation/src/main/java/com/example/lost_android/ui/component/entry/EntryFragment.kt
@@ -0,0 +1,38 @@
+package com.example.lost_android.ui.component.entry
+
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.example.lost_android.ui.base.BaseFragment
+import com.example.lost_android.viewmodel.EntryViewModel
+import com.example.presentation.R
+import com.example.presentation.databinding.FragmentEntryBinding
+
+class EntryFragment : BaseFragment<FragmentEntryBinding>(R.layout.fragment_entry) {
+    private val entryViewModel by activityViewModels<EntryViewModel>()
+    override fun createView() {
+        binding.entryFragment = this
+        setTitle()
+        isLost()
+    }
+
+    private fun setTitle() {
+        binding.titleTxt.text = entryViewModel.title.value
+    }
+
+    private fun isLost() {
+        if (entryViewModel.title.value != getString(R.string.lostEntry)) with(binding) {
+            imageEntry.text = getString(R.string.findImageEntry)
+            imageEntryTxt.text = getString(R.string.findImageEntry)
+            securityService.visibility = View.INVISIBLE
+            whatSecurityService.visibility = View.INVISIBLE
+            isSafeSwitch.visibility = View.INVISIBLE
+        }
+    }
+
+    fun click(view: View) {
+        when (view.id) {
+            R.id.backBtn, R.id.backTxt -> requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.entryContainer, EntryCategoryFragment()).commit()
+        }
+    }
+}
