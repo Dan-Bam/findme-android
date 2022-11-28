722ec57479537e1d160fc51bcf35b3dcdaa46b70
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-11-28 19:43:34 +0900

    카테고리 선택시 viewModel에 저장

--- presentation/src/main/java/com/example/lost_android/ui/component/entry/EntryCategoryFragment.kt
+++ presentation/src/main/java/com/example/lost_android/ui/component/entry/EntryCategoryFragment.kt
@@ -10,24 +10,12 @@ import com.example.presentation.databinding.FragmentEntryCategoryBinding
 class EntryCategoryFragment :
     BaseFragment<FragmentEntryCategoryBinding>(R.layout.fragment_entry_category) {
     private lateinit var categories: List<View>
+    private lateinit var categoryText: List<String>
     private val entryViewModel by activityViewModels<EntryViewModel>()
 
     override fun createView() {
         binding.entryCategoryFragment = this
-        categories = listOf(
-            binding.phoneHolder,
-            binding.laptopHolder,
-            binding.padHolder,
-            binding.wearHolder,
-            binding.earPhoneHolder,
-            binding.ringHolder,
-            binding.handRingHolder,
-            binding.neckRingHolder,
-            binding.watchHolder,
-            binding.outerHolder,
-            binding.bagHolder,
-            binding.walletHolder
-        )
+        initCategoryList()
         setTitle()
     }
 
@@ -43,16 +31,48 @@ class EntryCategoryFragment :
                     .replace(R.id.entryContainer, EntryFragment()).commit()
             }
             else -> {
-                categories.forEach {
+                categories.forEachIndexed { index, v ->
                     binding.nextBtn.isEnabled = true
                     binding.nextBtn.isActivated = true
-                    if (view == it) {
-                        it.setBackgroundResource(R.drawable.bg_category_select)
+                    if (view == v) {
+                        v.setBackgroundResource(R.drawable.bg_category_select)
+                        entryViewModel.setCategory(categoryText[index])
                     } else {
-                        it.setBackgroundResource(R.drawable.bg_category)
+                        v.setBackgroundResource(R.drawable.bg_category)
                     }
                 }
             }
         }
+    }
+
+    private fun initCategoryList() {
+        categories = listOf(
+            binding.phoneHolder,
+            binding.laptopHolder,
+            binding.padHolder,
+            binding.wearHolder,
+            binding.earPhoneHolder,
+            binding.ringHolder,
+            binding.handRingHolder,
+            binding.neckRingHolder,
+            binding.watchHolder,
+            binding.outerHolder,
+            binding.bagHolder,
+            binding.walletHolder
+        )
+        categoryText = listOf(
+            getString(R.string.phone),
+            getString(R.string.laptop),
+            getString(R.string.pad),
+            getString(R.string.wear),
+            getString(R.string.earphone),
+            getString(R.string.ring),
+            getString(R.string.handRing),
+            getString(R.string.neckRing),
+            getString(R.string.watch),
+            getString(R.string.outer),
+            getString(R.string.bag),
+            getString(R.string.wallet),
+        )
     }
 }
